c4ab194c44c1d1ea41f158086430041e8d1c2e55
Brendan Boyle <brendancboyle@me.com>
2015-02-13 18:17:59 -0500

    Switched to static tool set

--- M-Scout 15/M-Scout 15/MasterViewController.swift
+++ M-Scout 15/M-Scout 15/MasterViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = NSMutableArray()
-
+    
+    var scoutingTools: NSMutableArray = ["123", "456"]
+    var teamResources: NSMutableArray = ["789", "101"]
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -25,10 +26,10 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+        //self.navigationItem.leftBarButtonItem = self.editButtonItem()
 
         let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
+        //self.navigationItem.rightBarButtonItem = addButton
         if let split = self.splitViewController {
             let controllers = split.viewControllers
             self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
@@ -39,19 +40,26 @@ class MasterViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    func insertNewObject(sender: AnyObject) {
-        objects.insertObject(NSDate(), atIndex: 0)
+    
+    /*
+    func addObject(value: NSString) {
+        objects.insertObject(value, atIndex: 0)
         let indexPath = NSIndexPath(forRow: 0, inSection: 0)
         self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
     }
+    */
 
     // MARK: - Segues
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow() {
-                let object = objects[indexPath.row] as NSDate
+                var object: NSString = "";
+                if indexPath.section == 0 {
+                    object = scoutingTools[indexPath.row] as NSString
+                } else if indexPath.section == 1 {
+                    object = teamResources[indexPath.row] as NSString
+                }
                 let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
@@ -62,27 +70,49 @@ class MasterViewController: UITableViewController {
 
     // MARK: - Table View
 
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return 2
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Scouting Tools"
+        } else if section == 1 {
+            return "Team Resources"
+        }
+        return ""
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        if section == 0 {
+            return scoutingTools.count
+        } else if section == 1 {
+            return teamResources.count
+        }
+        return 0
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
-
-        let object = objects[indexPath.row] as NSDate
-        cell.textLabel!.text = object.description
+        if indexPath.section == 0 {
+            let object = scoutingTools[indexPath.row] as NSString
+            cell.textLabel!.text = object.description
+            cell.imageView?.image = UIImage(named: "MPArorLogo")
+        } else if indexPath.section == 1 {
+            let object = teamResources[indexPath.row] as NSString
+            cell.textLabel!.text = object.description
+        }
+        
         return cell
     }
 
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
+        return false
     }
 
+    /*
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             objects.removeObjectAtIndex(indexPath.row)
@@ -91,6 +121,8 @@ class MasterViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
+    */
+    
 
 
 }
