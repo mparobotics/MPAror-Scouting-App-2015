efb0fbdb894c9e14b0c566c450ee738edf72c141
Brendan Boyle <brendancboyle@me.com>
2015-02-19 01:28:49 -0500

    Fixed rotation crash bug

--- M-Scout 15/M-Scout 15/MasterViewController.swift
+++ M-Scout 15/M-Scout 15/MasterViewController.swift
@@ -67,8 +67,8 @@ class MasterViewController: UITableViewController {
                 
                 let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
                 controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-                controller.navigationItem.leftItemsSupplementBackButton = true
+                //controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                //controller.navigationItem.leftItemsSupplementBackButton = true
             }
         }
     }
